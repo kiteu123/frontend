359f0bd79c4b0c2018a029669495026c45dfd505
kiteu <kiteu@naver.com>
2025-11-14 13:33:19 +0900

    1114

---
+++ React/nasmedia/src/components/SectionOne.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SectionOne() {
+  return (
+    <div id="section1" className="section">
+      <div className="sub-txt">
+        <h3 className="sub-txt1">MORE THAN EXPECTED!</h3>
+        <h1 className="sub-txt2">No1. Digital Media Rep.</h1>
+      </div>
+    </div>
+  );
+}
